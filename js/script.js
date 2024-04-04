681b969694513f6698f9fae2eb6244d756473889
Salmen05 <miguelrangelsalmenevangelista@gmail.com>
2024-04-04 16:46:43 -0300

    sistema arcaico de drive

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+function login() {
 
+}
+function mostrarProcessando() {
+    var divProcessando = document.createElement("div");
+    divProcessando.id = "processandoDiv";
+    divProcessando.style.position = "fixed";
+    divProcessando.style.top = "85%";
+    divProcessando.style.left = "16.5%";
+    divProcessando.style.transform = "translate(-50%, -50%)";
+    divProcessando.innerHTML =
+        '<img src="./img/loading.gif" width="200px" alt="Processando..." title="Processando...">';
+    tobias.appendChild(divProcessando);
+}
+function esconderProcessando() {
+    const divProcessando = document.getElementById("processandoDiv");
+    if (divProcessando) {
+        tobias.removeChild(divProcessando);
+    }
+}
