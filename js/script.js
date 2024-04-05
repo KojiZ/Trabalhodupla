0fb67337c5c36fa16dad9901900f4aa87cafefb5
Miguel Salmen <miguelrangelsalmenevangelista@gmail.com>
2024-04-05 08:39:22 -0300

    Login funcional

--- js/script.js
+++ js/script.js
@@ -1,12 +1,75 @@
 function login() {
-
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+    const log = document.getElementById("log");
+    const divLoading = document.getElementById("tobias");
+    if (email === "" && senha === "") {
+        log.style.display = "block";
+        log.innerHTML = "Por favor, preencha os campos acima";
+        log.classList.remove("alert-light");
+        log.classList.add("alert-warning");
+        return;
+    } else if (email === "") {
+        log.style.display = "block";
+        log.innerHTML = "Por favor, preencha o campo de e-mail.";
+        log.classList.remove("alert-light");
+        log.classList.add("alert-warning");
+        return;
+    } else if (senha === "") {
+        log.style.display = "block";
+        log.innerHTML = "Preencha o campo de senha";
+        log.classList.remove("alert-light");
+        log.classList.add("alert-warning");
+        return;
+    } else if (senha.length < 6) {
+        log.style.display = "block";
+        log.innerHTML = "A senha deve conter no mínimo 6 dígitos";
+        log.classList.remove("alert-light");
+        log.classList.add("alert-warning");
+        return;
+    } else {
+        log.style.display = "none";
+    }
+    mostrarProcessando();
+    fetch("./login.php", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body:
+            "email=" +
+            encodeURIComponent(email) +
+            "&senha=" +
+            encodeURIComponent(senha),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.success) {
+                console.log(data);
+                setTimeout(function () {
+                    window.location.href = "./dashboard.php";
+                }, 2000);
+                log.style.display = "block";
+                log.classList.remove("alert-danger");
+                log.classList.remove("alert-light");
+                log.classList.add("alert-success");
+                log.innerHTML = data.message;
+            } else {
+                console.log(data);
+                log.style.display = "block";
+                log.classList.remove("alert-light");
+                log.classList.add("alert-danger");
+                log.innerHTML = data.message;
+            }
+            esconderProcessando();
+        })
 }
 function mostrarProcessando() {
     var divProcessando = document.createElement("div");
     divProcessando.id = "processandoDiv";
     divProcessando.style.position = "fixed";
     divProcessando.style.top = "85%";
-    divProcessando.style.left = "16.5%";
+    divProcessando.style.left = "50%";
     divProcessando.style.transform = "translate(-50%, -50%)";
     divProcessando.innerHTML =
         '<img src="./img/loading.gif" width="200px" alt="Processando..." title="Processando...">';
